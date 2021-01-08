38a4fb91de14d212e2e8336fa96417162f166b15
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-08 21:16:08 +0200

    s

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+function palindromeCheck(x){
+    
+    let result = '';
+    let y = x.toLowerCase()
+    for(let i = x.length; i > 0; i--){
+        result += y[i-1]
+    }
+    if (result === y){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+    console.log(result)
+}
+
+palindromeCheck('Bob')
