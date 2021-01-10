863f8c34d3efb101e615706c1c851b80cf62cff5
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-10 20:46:23 +0200

    practice

---
+++ map.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 2, 3];
+const arr2 = arr1.map(function(num){
+    return num * 2;
+});
+console.log(arr2)
+
+
+
---
+++ obj.js
@@ -0,0 +1,17 @@
+const obj = {
+    name: 'Arsenii',
+    surname: 'Shchastlyvyi',
+    age: 18
+};
+
+function reverse (value){
+    let res = '';
+    len = obj.name.length
+    for(let i = 1; i <= len; i++){
+        res += obj.name[len - i];
+    }
+    obj.name = res;
+}
+
+reverse(obj.name);
+console.log(obj);
