d1173c78daf9efd76ca082de3a7014fa523150ab
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-19 23:24:34 +0200

    Create fizzBuzz.js

---
+++ Javascript/fizzBuzz.js
@@ -0,0 +1,20 @@
+function fizzBuzz(num){
+    for (let i = 1; i <= num; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log(`${i}: fizzbuzz`);
+        }
+        else if (i % 3 === 0){
+            console.log(`${i}: fizz`);
+        }
+        else if (i % 5 === 0){
+            console.log(`${i}: buzz`);
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+
+const x = 12;
+fizzBuzz(x);
+
