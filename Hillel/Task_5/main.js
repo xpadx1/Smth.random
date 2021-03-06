0dd4055d767c6df471e853758b070fbbf41c5a0d
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-03-06 13:23:48 +0200

    task5.1

---
+++ Hillel/Task_5/main.js
@@ -0,0 +1,57 @@
+/*1*/
+function random(max){
+    let result = new Array;
+    for(let i = 0; i < max; i++){
+        result.push(Math.floor(Math.random()*Math.floor(max)));
+    }
+    return result;
+}
+
+/*2*/
+function compare(arr1,arr2){
+    arr1sum = 0;
+    arr2sum = 0;
+    arr1.forEach(function(arr, index){
+        if(typeof arr1[index] === 'number'){ 
+            arr1sum += arr1[index];
+        }});
+    arr2.forEach(function(arr, index){
+        if(typeof arr2[index] === 'number'){ 
+            arr2sum += arr2[index];
+        }});
+    console.log(arr1, arr2);
+    console.log(arr1sum, arr2sum);
+    if(arr1sum < arr2sum) {return `The biggest array: ${arr2}`}
+    else {return `The biggest array: ${arr1}`};
+}
+console.log(compare(random(10), random(10)));
+
+/*3*/
+function doMath(x, znak, y){
+    switch(znak){
+        case '+': return x + y;
+        case '-': return x - y;
+        case '*': return x * y;
+        case '/': return x / y;
+        case '%': return x % y;
+        case '^': return x ^ y;
+    }
+}
+console.log('Result: ' + doMath(2, '-', 3));
+
+/*4*/
+function remove(string, letters){
+    for(let i = 0; i <= string.length; i++){
+        letters.forEach(function(arr, index){
+            if(string[i] === letters[index]){
+                string = string.replace(letters[index], '') 
+                i--
+            };
+        });
+    }
+    return string;
+}
+console.log(remove('Let`s suck some dicks', ['s', 'e']))
+
+
+
